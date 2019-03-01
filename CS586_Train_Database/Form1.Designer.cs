7793634b1a5f875055130944efd837688fced84f
Michael Humphrey <mhumphree@gmail.com>
2019-03-01 00:07:21 -0800

    Created initial prototype to get data

--- CS586_Train_Database/Form1.Designer.cs
+++ CS586_Train_Database/Form1.Designer.cs
@@ -29,25 +29,106 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.status_label = new System.Windows.Forms.Label();
+            this.connect_button = new System.Windows.Forms.Button();
+            this.load_stations_button = new System.Windows.Forms.Button();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.find_button = new System.Windows.Forms.Button();
+            this.search_box = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.Size = new System.Drawing.Size(60, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Hello, world!";
+            this.label1.Text = "Status:";
+            // 
+            // status_label
+            // 
+            this.status_label.AutoSize = true;
+            this.status_label.Location = new System.Drawing.Point(78, 9);
+            this.status_label.Name = "status_label";
+            this.status_label.Size = new System.Drawing.Size(117, 20);
+            this.status_label.TabIndex = 1;
+            this.status_label.Text = "Not connected.";
+            // 
+            // connect_button
+            // 
+            this.connect_button.Location = new System.Drawing.Point(12, 32);
+            this.connect_button.Name = "connect_button";
+            this.connect_button.Size = new System.Drawing.Size(134, 34);
+            this.connect_button.TabIndex = 2;
+            this.connect_button.Text = "Connect";
+            this.connect_button.UseVisualStyleBackColor = true;
+            this.connect_button.Click += new System.EventHandler(this.connect_button_Click);
+            // 
+            // load_stations_button
+            // 
+            this.load_stations_button.Enabled = false;
+            this.load_stations_button.Location = new System.Drawing.Point(12, 125);
+            this.load_stations_button.Name = "load_stations_button";
+            this.load_stations_button.Size = new System.Drawing.Size(134, 34);
+            this.load_stations_button.TabIndex = 3;
+            this.load_stations_button.Text = "Load Stations";
+            this.load_stations_button.UseVisualStyleBackColor = true;
+            this.load_stations_button.Click += new System.EventHandler(this.load_stations_button_Click);
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Location = new System.Drawing.Point(12, 166);
+            this.grid.Name = "grid";
+            this.grid.ReadOnly = true;
+            this.grid.RowTemplate.Height = 28;
+            this.grid.Size = new System.Drawing.Size(505, 268);
+            this.grid.TabIndex = 4;
+            // 
+            // find_button
+            // 
+            this.find_button.Enabled = false;
+            this.find_button.Location = new System.Drawing.Point(391, 126);
+            this.find_button.Name = "find_button";
+            this.find_button.Size = new System.Drawing.Size(126, 34);
+            this.find_button.TabIndex = 5;
+            this.find_button.Text = "Find";
+            this.find_button.UseVisualStyleBackColor = true;
+            this.find_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // search_box
+            // 
+            this.search_box.Enabled = false;
+            this.search_box.Location = new System.Drawing.Point(242, 130);
+            this.search_box.Name = "search_box";
+            this.search_box.Size = new System.Drawing.Size(143, 26);
+            this.search_box.TabIndex = 6;
+            this.search_box.KeyDown += new System.Windows.Forms.KeyEventHandler(this.search_box_KeyDown);
+            this.search_box.KeyUp += new System.Windows.Forms.KeyEventHandler(this.search_box_KeyUp);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(529, 446);
+            this.Controls.Add(this.search_box);
+            this.Controls.Add(this.find_button);
+            this.Controls.Add(this.grid);
+            this.Controls.Add(this.load_stations_button);
+            this.Controls.Add(this.connect_button);
+            this.Controls.Add(this.status_label);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "CS586 Train Database Application";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +137,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label status_label;
+        private System.Windows.Forms.Button connect_button;
+        private System.Windows.Forms.Button load_stations_button;
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Button find_button;
+        private System.Windows.Forms.TextBox search_box;
     }
 }
 
--- CS586_Train_Database/Form1.cs
+++ CS586_Train_Database/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,136 @@ namespace CS586_Train_Database
 {
     public partial class Form1 : Form
     {
+        NpgsqlConnection conn;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void connect_button_Click(object sender, EventArgs e)
+        {
+            // Build connection string
+            NpgsqlConnectionStringBuilder builder = new NpgsqlConnectionStringBuilder();
+            builder.Host = "dbclass.cs.pdx.edu";
+            builder.Database = "w19wdb19";
+            builder.Username = "w19wdb19";
+            builder.Passfile = GetPassfilePath();
+            builder.SslMode = SslMode.Require;
+            builder.TrustServerCertificate = true;
+
+            // Check for password file
+            if (builder.Passfile == null)
+            {
+                MessageBox.Show(@"PostgreSQL password file not found! Please create a password file at %APPDATA%\postgresql\pgpass.conf (where %APPDATA% refers to the Application Data subdirectory in the user's profile).", "ERROR: No password file found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                status_label.Text = "No password file!";
+                status_label.ForeColor = Color.Red;
+                return;
+            }
+
+            update_status("Connecting...", Color.DarkKhaki);
+            connect_button.Enabled = false;
+
+            conn = new NpgsqlConnection(builder.ConnectionString);
+
+            try
+            {
+                await conn.OpenAsync();
+                update_status("Connected.", Color.Green);
+                connect_button.Text = "Connected";
+                load_stations_button.Enabled = true;
+                find_button.Enabled = true;
+                search_box.Enabled = true;
+            }
+            catch (NpgsqlException)
+            {
+                update_status("Unable to connect.", Color.Red);
+                MessageBox.Show("Unable to connect to database. Please make sure VPN is enabled and try again.", "ERROR: Unable to connect to database", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                connect_button.Enabled = true;
+            }
+        }
+
+        private string GetPassfilePath()
+        {
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "postgresql", "pgpass.conf");
+            if (File.Exists(path))
+            {
+                return path;
+            }
+
+            path = Path.Combine(Directory.GetCurrentDirectory(), "pgpass.conf");
+            if (File.Exists(path))
+            {
+                return path;
+            } else
+            {
+                return null;
+            }
+        }
+
+        private void update_status(string message, Color? color = null)
+        {
+            status_label.Text = message;
+            status_label.ForeColor = color ?? Color.Black;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (conn != null && conn.State != System.Data.ConnectionState.Closed)
+            {
+                conn.Close();
+            }
+        }
+
+        private void load_stations_button_Click(object sender, EventArgs e)
+        {
+            using (var cmd = new NpgsqlCommand("SELECT * FROM train.station", conn))
+            {
+                DataSet ds = new DataSet();
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+                da.Fill(ds);
+                grid.DataSource = ds.Tables[0];
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            search();
+        }
+
+        private void search_box_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                search();
+                e.Handled = e.SuppressKeyPress = true;
+            }
+        }
+
+        private void search()
+        {
+            string search = search_box.Text;
+
+            using (var cmd = new NpgsqlCommand())
+            {
+                cmd.Connection = conn;
+                cmd.CommandText = "SELECT * FROM train.station WHERE UPPER(code) = UPPER(@code) OR name ILIKE '%' || @name || '%' OR city ILIKE '%' || @city || '%'";
+                cmd.Parameters.AddWithValue("city", search);
+                cmd.Parameters.AddWithValue("code", search);
+                cmd.Parameters.AddWithValue("name", search);
+                DataSet ds = new DataSet();
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+                da.Fill(ds);
+                grid.DataSource = ds.Tables[0];
+            }
+        }
+
+        private void search_box_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                e.Handled = e.SuppressKeyPress = true;
+            }
         }
     }
 }
